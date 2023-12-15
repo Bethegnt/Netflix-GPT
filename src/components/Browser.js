b39a1dbdea36ccdb2644b8d8058ab98da23eeaa3
bethegnt <tyagigagan30@gmail.com>
2023-12-15 19:58:29 +0530

    TMDB API data fetch && build MainContainer

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header';
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 const Browser = () => {
   useNowPlayingMovies();
   return (
     <>
     <Header/>
+    <MainContainer/>
+    <SecondaryContainer/>
     </>
   );
   };
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -9,9 +9,12 @@ const movieSlice = createSlice({
     reducers:{
         addNowPlayingMovies:(state,action)=>{
             state.nowPlayingMovies = action.payload;
+        },
+        addTrailerVideo:(state,action)=>{
+            state.trailerVideo = action.payload;
         }
     }
 });
 
-export const {addNowPlayingMovies} = movieSlice.actions;
+export const {addNowPlayingMovies, addTrailerVideo} = movieSlice.actions;
 export default movieSlice.reducer; 
